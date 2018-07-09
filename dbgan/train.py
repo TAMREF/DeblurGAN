3623130d1cce2f03213706d72a2bacbcbe38b1eb
TAMREF <sluggeryck@naver.com>
2018-07-10 08:08:56 +0900

    gray_contrast_11

--- dbgan/train.py
+++ dbgan/train.py
@@ -13,89 +13,91 @@ from keras.optimizers import Adam
 BASE_DIR = 'weights/'
 
 def save_all_weights(d, g, epoch_number, current_loss):
-	now = datetime.datetime.now()
-	save_dir = os.path.join(BASE_DIR, 'gray_contrast')
-	if not os.path.exists(save_dir):
-		os.makedirs(save_dir)
-	g.save_weights(os.path.join(save_dir, 'generator_{}.h5'.format(epoch_number)), True)
-	d.save_weights(os.path.join(save_dir, 'discriminator_{}.h5'.format(epoch_number)), True)
+    now = datetime.datetime.now()
+    save_dir = os.path.join(BASE_DIR, 'gray_contrast')
+    if not os.path.exists(save_dir):
+    	os.makedirs(save_dir)
+    g.save_weights(os.path.join(save_dir, 'generator_{}.h5'.format(epoch_number)), True)
+    d.save_weights(os.path.join(save_dir, 'discriminator_{}.h5'.format(epoch_number)), True)
 
 
 def train_multiple_outputs(n_images, batch_size, epoch_num, critic_updates=5, dodebug=False):
-	data = load_images('../images', n_images)
-	y_train, x_train = data['B'], data['A']
+    data = load_images('../images', n_images)
+    y_train, x_train = data['B'], data['A']
+    weight_dir = 'gray_contrast'
+    epoch_already = 7
+    g = generator_model()
+    d = discriminator_model()
+    g.load_weights('./{}/{}/generator_{}.h5'.format(BASE_DIR,weight_dir, epoch_already))
+    d.load_weights('./{}/{}/discriminator_{}.h5'.format(BASE_DIR,weight_dir, epoch_already))
+    d_on_g = generator_containing_discriminator_multiple_outputs(g, d)
 
-	g = generator_model()
-	d = discriminator_model()
-	d_on_g = generator_containing_discriminator_multiple_outputs(g, d)
+    d_opt = Adam(lr=1E-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+    d_on_g_opt = Adam(lr=1E-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
 
-	d_opt = Adam(lr=1E-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-	d_on_g_opt = Adam(lr=1E-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+    d.trainable = True
+    d.compile(optimizer=d_opt, loss=wasserstein_loss)
+    d.trainable = False
+    loss = [perceptual_loss, wasserstein_loss]
+    loss_weights = [100, 1]
+    d_on_g.compile(optimizer=d_on_g_opt, loss=loss, loss_weights=loss_weights)
+    d.trainable = True
 
-	d.trainable = True
-	d.compile(optimizer=d_opt, loss=wasserstein_loss)
-	d.trainable = False
-	loss = [perceptual_loss, wasserstein_loss]
-	loss_weights = [100, 1]
-	d_on_g.compile(optimizer=d_on_g_opt, loss=loss, loss_weights=loss_weights)
-	d.trainable = True
+    output_true_batch, output_false_batch = np.ones((batch_size, 1)), np.zeros((batch_size, 1))
+    for epoch in range(epoch_already,epoch_num+epoch_already):
+    	print('epoch: {}/{}'.format(epoch, epoch_num))
+    	print('batches: {}'.format(x_train.shape[0] / batch_size))
 
-	output_true_batch, output_false_batch = np.ones((batch_size, 1)), np.zeros((batch_size, 1))
+    	permutated_indexes = np.random.permutation(x_train.shape[0])
 
-	for epoch in range(epoch_num):
-		print('epoch: {}/{}'.format(epoch, epoch_num))
-		print('batches: {}'.format(x_train.shape[0] / batch_size))
+    	d_losses = []
+    	d_on_g_losses = []
+    	if dodebug : print('Flag #1 : Loop')
+    	if dodebug : print(x_train.shape)
+    	for index in range(int(x_train.shape[0] / batch_size)):
+    		if dodebug : print(index)
+    		batch_indexes = permutated_indexes[index*batch_size:(index+1)*batch_size]
+    		image_blur_batch = x_train[batch_indexes]
+    		image_full_batch = y_train[batch_indexes]
+    		if dodebug : print('Flag #2 : x_train, y_train')
+    		
+    		generated_images = g.predict(x=image_blur_batch, batch_size=batch_size)
+    		if dodebug : print('Flag #3 : g.predict')
+    		
+    		for i in range(critic_updates):
+    			if dodebug : print('Flag #4 : critic training : {}'.format(i))
+    			d_loss_real = d.train_on_batch(image_full_batch, output_true_batch)
+    			d_loss_fake = d.train_on_batch(generated_images, output_false_batch)
+    			d_loss = 0.5 * np.add(d_loss_fake, d_loss_real)
+    			d_losses.append(d_loss)
+    		print('epoch {} batch {} d_loss : {}'.format(epoch+1, index+1, np.mean(d_losses)))
 
-		permutated_indexes = np.random.permutation(x_train.shape[0])
+    		d.trainable = False
 
-		d_losses = []
-		d_on_g_losses = []
-		if dodebug : print('Flag #1 : Loop')
-		if dodebug : print(x_train.shape)
-		for index in range(int(x_train.shape[0] / batch_size)):
-			if dodebug : print(index)
-			batch_indexes = permutated_indexes[index*batch_size:(index+1)*batch_size]
-			image_blur_batch = x_train[batch_indexes]
-			image_full_batch = y_train[batch_indexes]
-			if dodebug : print('Flag #2 : x_train, y_train')
-			
-			generated_images = g.predict(x=image_blur_batch, batch_size=batch_size)
-			if dodebug : print('Flag #3 : g.predict')
-			
-			for i in range(critic_updates):
-				if dodebug : print('Flag #4 : critic training : {}'.format(i))
-				d_loss_real = d.train_on_batch(image_full_batch, output_true_batch)
-				d_loss_fake = d.train_on_batch(generated_images, output_false_batch)
-				d_loss = 0.5 * np.add(d_loss_fake, d_loss_real)
-				d_losses.append(d_loss)
-			print('epoch {} batch {} d_loss : {}'.format(epoch+1, index+1, np.mean(d_losses)))
+    		d_on_g_loss = d_on_g.train_on_batch(image_blur_batch, [image_full_batch, output_true_batch])
+    		d_on_g_losses.append(d_on_g_loss)
+    		print('epoch {} batch {} d_on_g_loss : {}'.format(epoch+1, index+1, d_on_g_loss))
 
-			d.trainable = False
+    		d.trainable = True
 
-			d_on_g_loss = d_on_g.train_on_batch(image_blur_batch, [image_full_batch, output_true_batch])
-			d_on_g_losses.append(d_on_g_loss)
-			print('epoch {} batch {} d_on_g_loss : {}'.format(epoch+1, index+1, d_on_g_loss))
+    	with open('log.txt', 'a') as f:
+    		f.write('{} - {} - {}\n'.format(epoch, np.mean(d_losses), np.mean(d_on_g_losses)))
 
-			d.trainable = True
-
-		with open('log.txt', 'a') as f:
-			f.write('{} - {} - {}\n'.format(epoch, np.mean(d_losses), np.mean(d_on_g_losses)))
-
-		save_all_weights(d, g, epoch, int(np.mean(d_on_g_losses)))
+    	save_all_weights(d, g, epoch, int(np.mean(d_on_g_losses)))
 
 
 @click.command()
 @click.option('--n_images', default=1024, help='Number of images to load for training')
-@click.option('--batch_size', default=64, help='Size of batch')
-@click.option('--epoch_num', default=12, help='Number of epochs for training')
-@click.option('--critic_updates', default=5, help='Number of discriminator training')
+@click.option('--batch_size', default=8, help='Size of batch')
+@click.option('--epoch_num', default=4, help='Number of epochs for training')
+@click.option('--critic_updates', default=10, help='Number of discriminator training')
 @click.option('--dodebug',default=False, help='activate dodebug print functions')
 
 
 def train_command(n_images, batch_size, epoch_num, critic_updates,dodebug):
-	return train_multiple_outputs(n_images, batch_size, epoch_num, critic_updates,dodebug)
+    return train_multiple_outputs(n_images, batch_size, epoch_num, critic_updates,dodebug)
 
 
 if __name__ == '__main__':
-	
-	train_command()
+    
+    train_command()
